209e4db9a8a5d74c62ef8698f28a70f3caf0a35a
DMITRII VORONETS <dmivron620@gmail.com>
2025-05-19 22:53:28 +0500

    8.3 Маркировки Pytest. Практика работы с pytest маркировкам

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,5 +1,5 @@
+import pytest
 from http import HTTPStatus
-
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
 from clients.users.public_users_client import get_public_users_client
@@ -8,6 +8,8 @@ from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
+@pytest.mark.authentication
+@pytest.mark.regression
 def test_login():
     # Инициализируем API-клиент для работы с пользователями и аутентификации
     public_users_client = get_public_users_client()
